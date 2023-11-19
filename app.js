a741df513bf590b22de46f27948fe4ae2e9a4ec1
Jun Noiri <63932655+junnoiri@users.noreply.github.com>
2023-11-19 14:39:41 -0600

    11/19

--- app.js
+++ app.js
@@ -15,117 +15,141 @@ app.use(express.static("static"));
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    },
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
 });
 
 app.get("/", async (req, res) => {
-    client.connect;
-    let mongoResult = await client
-        .db("quebec-database")
-        .collection("drinks")
-        .find()
-        .toArray();
-    // console.log("get/: ", result);
-    console.log(mongoResult);
-    // ('res.send("here for a second: " + result[0].name)');
-    res.render("index", {
-        drinkOptions: mongoResult,
-        selectedFreezingTime: 0,
-    });
+  client.connect;
+
+  // Get the selected drink name and freezing time from the query parameters
+  const selectedDrink = req.query.selectedDrink;
+  const minutes = req.query.selectedFreezingMinute;
+  const seconds = req.query.selectedFreezingSecond;
+
+  let mongoResult = await client
+    .db("quebec-database")
+    .collection("drinks")
+    .find()
+    .toArray();
+  res.render("index", {
+    drinkOptions: mongoResult,
+    selectedDrink: selectedDrink,
+    selectedFreezingMinute: minutes,
+    selectedFreezingSecond: seconds,
+  });
 });
 
 app.post("/addCustomDrink", async (req, res) => {
-    try {
-        //get the new dev name
-        // console.log("body: ", req.body);
-        // console.log("user Name: ", req.body.devName);
+  try {
+    const customDrinkName = req.body.customDrinkName;
+    const freezingTime = req.body.freezingTime;
 
-        const customDrinkName = req.body.customDrinkName;
-        if (!customDrinkName) {
-            return res.status(400).send("Drink name is required.");
-        }
-
-        const collection = client.db("quebec-database").collection("drinks");
-
-        // Insert the custom drink into the database
-        await collection.insertOne({
-            drinkName: customDrinkName,
-        });
-        res.redirect("/");
-    } catch (error) {
-        console.error(error);
-        res.status(500).send(
-            "An error occurred while adding the custom drink.",
-        );
+    // Added: Check if the input values are valid
+    if (!customDrinkName || !freezingTime || isNaN(parseInt(freezingTime))) {
+      return res
+        .status(400)
+        .send("Invalid input. Drink name and freezing time are required.");
     }
+
+    const collection = client.db("quebec-database").collection("drinks");
+
+    // Modified: Check if same drink name already exists
+    const existingDrink = await collection.findOne({
+      drinkName: customDrinkName,
+    });
+    if (existingDrink) {
+      return res
+        .status(400)
+        .send("This drink already exists. Choose a different name.");
+    }
+
+    // Insert the custom drink into the database
+    await collection.insertOne({
+      drinkName: customDrinkName,
+      freezingTime: String(freezingTime),
+    });
+
+    res.redirect("/");
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("An error occurred while adding the custom drink.");
+  }
 });
 
 app.post("/selectDrink", async (req, res) => {
-    try {
-        const selectedDrink = req.body.selectedDrink;
-        console.log("selectedDrink:", selectedDrink);
+  try {
+    const selectedDrink = req.body.selectedDrink;
+    // console.log("selectedDrink:", selectedDrink);
 
-        if (selectedDrink === "select") {
-            return res.status(400).send("Please select a drink.");
-        }
-
-        // Assuming you have a MongoDB collection named "drinks"
-        const collection = client.db("quebec-database").collection("drinks");
-
-        // Find the selected drink's freezing time
-        const drink = await collection.findOne({ drinkName: selectedDrink });
-        console.log("drink:", drink);
-
-        if (drink) {
-            // Get the freezing time of the selected drink
-            const freezingTime = drink.freezingTime;
-
-            // Get the drink options
-            let mongoResult = await collection.find().toArray();
-            // console.log(mongoResult);
-
-            // Send the freezing time as a response
-            res.render("index", {
-                drinkOptions: mongoResult,
-                selectedFreezingTime: freezingTime,
-            });
-        } else {
-            // Handle the case where the drink is not found
-            res.status(404).send("Selected drink not found in the database.");
-        }
-    } catch (error) {
-        console.error(error);
-        res.status(500).send("An error occurred while processing the request.");
+    if (selectedDrink === "select") {
+      return res.status(400).send("Please select a drink.");
     }
+
+    // Assuming you have a MongoDB collection named "drinks"
+    const collection = client.db("quebec-database").collection("drinks");
+
+    // Find the selected drink's freezing time
+    const drink = await collection.findOne({ drinkName: selectedDrink });
+    // console.log("drink:", drink);
+
+    if (drink) {
+      // Get the freezing time of the selected drink
+      const freezingTime = drink.freezingTime;
+
+      // Convert freezing time to minutes and seconds
+      const minutes = Math.floor(freezingTime);
+      const seconds = Math.ceil((freezingTime - minutes) * 60);
+
+      // console.log("minutes:", minutes);
+      // console.log("seconds:", seconds);
+
+      // Set redirect path and query parameters
+      const redirectToPath = "/";
+      const redirectToQuery = `?selectedDrink=${encodeURIComponent(
+        selectedDrink
+      )}&selectedFreezingMinute=${encodeURIComponent(
+        minutes
+      )}&selectedFreezingSecond=${encodeURIComponent(seconds)}`;
+
+      // redirect
+      res.redirect(redirectToPath + redirectToQuery);
+    } else {
+      // Handle the case where the drink is not found
+      res.status(404).send("Selected drink not found in the database.");
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("An error occurred while adding the custom drink.");
+  }
 });
 
 app.get("/mail", (req, res) => {
-    var transporter = nodemailer.createTransport({
-        service: "gmail",
-        auth: {
-            user: process.env.grant_email,
-            pass: process.env.grant_pass,
-        },
-    });
+  var transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.grant_email,
+      pass: process.env.grant_pass,
+    },
+  });
 
-    var mailOptions = {
-        from: process.env.grant_email,
-        to: process.env.grant_email,
-        text: "Test email",
-    };
-    transporter.sendMail(mailOptions, function (error, info) {
-        if (error) {
-            console.log(error);
-        } else {
-            console.log("Email sent: " + info.response);
-        }
-    });
+  var mailOptions = {
+    from: process.env.grant_email,
+    to: process.env.grant_email,
+    text: "Test email",
+  };
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Email sent: " + info.response);
+    }
+  });
 });
 
 app.listen(port, () =>
-    console.log(`Server is running...on http://localhost:${port}`),
+  console.log(`Server is running...on http://localhost:${port}`)
 );
