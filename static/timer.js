76614bd435a9abc361f9fb2ffe0fd2c1085cc0ac
Jun Noiri <63932655+junnoiri@users.noreply.github.com>
2023-11-19 14:39:41 -0600

    11/19

--- static/timer.js
+++ static/timer.js
@@ -1,7 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
   const countdownDisplay = document.getElementById("countdownDisplay");
   const countupDisplay = document.getElementById("countupDisplay");
-  const timeInput = document.getElementById("timeInput");
+  const minutesInput = document.getElementById("minutesInput");
+  const secondsInput = document.getElementById("secondsInput");
   const startButton = document.getElementById("startButton");
   const pauseButton = document.getElementById("pauseButton");
 
@@ -14,20 +15,30 @@ document.addEventListener("DOMContentLoaded", function () {
   let isPaused = false;
   let hasFinished = false;
 
-  timeInput.addEventListener("input", function () {
+  // Added: Processing when the minutes input field is changed
+  minutesInput.addEventListener("input", function () {
     pauseButton.disabled = isCountingDown || isCountingUp;
   });
 
+  // Added: Processing when the seconds input field is changed
+  secondsInput.addEventListener("input", function () {
+    pauseButton.disabled = isCountingDown || isCountingUp;
+  });
+
+  // Fixed: Processing when pressing the start button and pause button
   startButton.addEventListener("click", function () {
-    const timeInMinutes = parseInt(timeInput.value);
-    if (!isNaN(timeInMinutes) && timeInMinutes > 0) {
+    const minutes = parseInt(minutesInput.value);
+    const seconds = parseInt(secondsInput.value);
+    const timeInSeconds = minutes * 60 + seconds;
+
+    if (!isNaN(timeInSeconds) && timeInSeconds > 0) {
       if (isCountingDown || isCountingUp) {
         clearInterval(countdownInterval);
         clearInterval(countupInterval);
         countupDisplay.textContent = "";
       }
       hasFinished = false;
-      remainingTime = timeInMinutes * 60;
+      remainingTime = timeInSeconds;
       startCountdown(remainingTime);
     }
   });
@@ -46,7 +57,7 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 
-  function startCountdown(timeInSeconds) {
+  function startCountdown() {
     if (isCountingDown) {
       clearInterval(countdownInterval);
     }
@@ -65,6 +76,24 @@ document.addEventListener("DOMContentLoaded", function () {
         startCountup();
       } else {
         updateCountdownDisplay(remainingTime);
+
+        // Set background color based on remaining time
+        document.querySelector(".area").style.backgroundColor = "#4e54c8";
+        document.body.style.backgroundColor = "#4e54c8";
+
+        const minutes = parseInt(minutesInput.value);
+        const seconds = parseInt(secondsInput.value);
+
+        if (remainingTime <= (minutes * 60 + seconds) / 2) {
+          document.querySelector(".area").style.backgroundColor = "#c8c04e";
+          document.body.style.backgroundColor = "#c8c04e";
+        }
+
+        if (remainingTime <= 10) {
+          document.querySelector(".area").style.backgroundColor = "#c84e4e";
+          document.body.style.backgroundColor = "#c84e4e";
+        }
+
         remainingTime--;
       }
     }, 1000);
@@ -88,6 +117,7 @@ document.addEventListener("DOMContentLoaded", function () {
       2,
       "0"
     )}`;
+    console.log(remainingTime);
   }
 
   function updateCountupDisplay(elapsedSeconds) {
--- static/ui-control.js
+++ static/ui-control.js
@@ -2,19 +2,13 @@ document.addEventListener("DOMContentLoaded", function () {
   const selectedDrink = document.getElementById("selectedDrink");
   const customDrinkSection = document.querySelector(".custom-drink-section");
 
-  const setFreezingTimeSection = document.querySelector(
-    ".set-freezing-time-section"
-  );
   customDrinkSection.style.display = "none";
-  setFreezingTimeSection.style.display = "none";
 
   selectedDrink.addEventListener("change", function () {
     if (selectedDrink.value === "Custom") {
       customDrinkSection.style.display = "block";
-      setFreezingTimeSection.style.display = "block";
     } else {
       customDrinkSection.style.display = "none";
-      setFreezingTimeSection.style.display = "none";
     }
   });
 
